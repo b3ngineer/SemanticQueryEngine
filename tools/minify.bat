667a612bf5d0c2529caa7bfaa5648daf17c14d4e
Ben Harris <ben.harris@netscape.com>
2013-12-30 08:04:07 -0700

    all tests passing except multiple agenda
    structure setup

---
+++ tools/minify.bat
@@ -0,0 +1,8 @@
+REM requires https://developers.google.com/closure/compiler/ 
+"C:\Program Files\Java\jre7\bin\java.exe" -jar ../lib/compiler.jar --compilation_level=SIMPLE_OPTIMIZATIONS --js=../src/core.js --js_output_file=../sqe.library.min.js
+
+"C:\Program Files\Java\jre7\bin\java.exe" -jar ../lib/compiler.jar --compilation_level=WHITESPACE_ONLY --formatting=pretty_print --js=../src/core.js --js_output_file=../sqe.library.js
+
+xcopy /s /y /r "..\sqe.library.min.js" "..\..\..\ual\sqe.library.min.js"
+pause
+
